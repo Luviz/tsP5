5b73979692756775302c4f70fb0835f29aed7a4f
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-18 16:34:20 +0100

    added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
-const path = require('path');
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    entry: './src/index.ts',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+            
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:"./src/index.html",
+            title:"something",
+            
+        })
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
