991076767a97e4d2cf38280c40049776826a40a0
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-18 16:58:43 +0100

    some update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import * as p5 from "p5";
+
+
+new p5((p: p5)=> {
+    p.setup = () => {
+        const canvas  = p.createCanvas(400, 400);
+        canvas.parent("p5")
+    }
+    p.draw = () => {
+        p.background(0)
+
+    }
+})
