98f55e163e458f3d4b3b90b89d9bff7cf6a9c0e7
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-21 00:26:08 +0100

    equilibrium

--- src/sketches/GravityII.ts
+++ src/sketches/GravityII.ts
@@ -4,42 +4,53 @@ import Planet from "../entities/Planet";
 export const sketch = new p5((p: p5) => {
     const objects: Planet[] = [];
 
-    function createPlant(color: string, size:number) {
+    function createPlant(color: string, size: number) {
         const planet = new Planet(p, p.createVector(0), size)
         planet.color = p.color(color)
+        planet.id = color;
         return planet;
     }
 
     p.setup = () => {
         const canvas = p.createCanvas(1600, 1600);
         canvas.parent("p5");
+        const initFvalue = 63.246//59.9998;
         
-        const p1 = createPlant("yellow", 100);
-        
+
+        const p1 = createPlant("yellow", 150);
+
         const p2 = createPlant("blue", 10);
         p2.setPostion(500, 0);
-        p2.ApplyForce(p.createVector(0,50))
+        p2.ApplyForce(p.createVector(0, -initFvalue))
 
-        
+
         const p3 = createPlant("red", 10);
         p3.setPostion(0, 500);
-        p3.ApplyForce(p.createVector(-50,0))
-        
-        objects.push(p1, p2, p3)
+        p3.ApplyForce(p.createVector(initFvalue, 0))
+
+
+        const p4 = createPlant("black", 10);
+        p4.setPostion(0, -500);
+        p4.ApplyForce(p.createVector(-initFvalue, 0))
+
+        const p5 = createPlant("orange", 10);
+        p5.setPostion(-500, 0);
+        p5.ApplyForce(p.createVector(0, initFvalue))
+
+        objects.push(p1, p2, p3, p4, p5)
 
         for (const obj of objects) {
             obj.EffectedBy.push(...objects.filter(o => obj !== o))
         }
     }
-    
+    let a = 1000
     p.draw = () => {
-        p.translate(p.width / 2, p.height / 2)
         p.background(200);
+        p.translate(p.width / 2, p.height / 2)
 
         for (const obj of objects) {
             obj.GetGravAcc();
         }
-
 
         for (const o of objects) {
             o.Update();
@@ -47,6 +58,6 @@ export const sketch = new p5((p: p5) => {
         }
 
         p.fill("grey");
-        p.circle(0,0,5)
+        p.circle(0, 0, 5);
     }
 })
