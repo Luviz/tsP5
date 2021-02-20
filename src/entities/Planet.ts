fcae76fb28e3fc0a95254b8146b40e4ce1d5aa23
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-21 00:00:02 +0100

    added History to plant

--- src/entities/Planet.ts
+++ src/entities/Planet.ts
@@ -3,6 +3,9 @@ import Walker from "./Walker";
 
 export default class Planet extends Walker {
     public EffectedBy: Walker[] = [];
+    public HistoryCount: number = 1000;
+    public History: Vector[] = [];
+    public id:string = ""
 
     PastEdge(){
 
@@ -17,5 +20,25 @@ export default class Planet extends Walker {
             force.mult(strength);
             this.ApplyForce(force)
         }
+    }
+
+    Draw(){
+        if (this.HistoryCount > 0){
+            this.History.unshift(this.postion.copy());
+            this.History = this.History.splice(0,this.HistoryCount);
+        }
+        super.Draw();
+        this.color.setAlpha(100)
+        this.p.push();
+        this.p.noFill();
+        this.p.stroke(this.color);
+        
+        this.p.strokeWeight(5);
+        this.p.beginShape();
+        for (const hist of this.History) {
+            this.p.vertex(hist.x, hist.y)
+        }
+        this.p.endShape();
+        this.p.pop();
     }
 }
