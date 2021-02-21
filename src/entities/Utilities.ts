2f49437dfe665b517182d9f2809f17dbb9428e90
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-21 20:10:59 +0100

    tracevaleu

--- src/entities/Utilities.ts
+++ src/entities/Utilities.ts
@@ -13,3 +13,21 @@ export function DrawArrow(p: p5, base: p5.Vector, vec: p5.Vector, myColor: strin
     p.triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
     p.pop();
 }
+
+export function traceValue(p:p5, val:number,c:string= "black", arr:number[] = []){
+    arr.push(val);
+    if (arr.length > p.width){ arr.splice(0,1)}
+
+    p.push();
+    p.stroke(p.color(c));
+    p.noFill();
+    p.beginShape();
+    for (let index = 0; index < arr.length; index++) {
+        p.vertex(index , arr[index]);
+    }
+    p.endShape();
+    p.strokeWeight(10)
+    p.point(arr.length-1, arr[arr.length-1]);
+    p.pop()
+    return arr;
+}
