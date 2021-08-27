bbd7078f84adb37c3bd465ec53305e26af3d6915
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-08-27 08:55:01 +0200

    color scheam fix

--- src/sketches/MandelbrotSetAsyncCalc.ts
+++ src/sketches/MandelbrotSetAsyncCalc.ts
@@ -4,7 +4,7 @@ export declare let matrix: any[][];
 export declare let window: any;
 
 function map(n: number, start1: number, stop1: number, start2: number, stop2: number, withinBounds?: boolean) {
-    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+    return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
 }
 
 function colorA(grey: number) {
@@ -16,14 +16,14 @@ function colorRGB(red: number, blue: number, green: number) {
 }
 
 
-export async function BuildMandArr(width = 600, height = 600, range = 2.00, offReal = -1.13, offImg=0.21) {
+export async function BuildMandArr(width = 600, height = 600, range = 2.00, offReal = -1.13, offImg = 0.21) {
     // await "aa";
     matrix = [[]];
     for (let y = 0; y < height; y++) {
         if (!matrix[y]) matrix[y] = [];
         for (let x = 0; x < width; x++) {
-            let _realComp = map(x, 0, width, -range+offReal, range+offReal);
-            let _imagComp = map(y, 0, height, -range+offImg, range+offImg);
+            let _realComp = map(x, 0, width, -range + offReal, range + offReal);
+            let _imagComp = map(y, 0, height, -range + offImg, range + offImg);
             // SetPixel(y, x, color);
             matrix[y][x] = GetPixColor(_realComp, _imagComp)
         }
@@ -32,29 +32,35 @@ export async function BuildMandArr(width = 600, height = 600, range = 2.00, offR
 
 
 const colorFrom = (value: number, lim: number) => {
-    if (window['valMax'] < value){
-        window['valMax'] = value 
-    }else {
-        window['valMax'] = 0 
+    const getGrey = (start: number, end: number, pos: number) => start + pos * (end - start);
+
+    const procent = map(value, 0, lim, 0, 1);
+
+    const gry = (p = procent) => getGrey(0x00, 0xff, p / (1 / 6));
+    const igry = (p = procent) => getGrey(0xff, 0x00, p / (1 / 6));
+
+    if (procent <= 1 / 6) {
+        return [0, 0, gry()];
+    } else if (procent > 1 / 6 && procent <= 2 / 6) {
+        return [0x00, gry(procent - 1 / 6), 0xff];
     }
-    const v = map(value, 0, lim, 0, 0x00ffff);
-    if (v < 0xff) {
-        return [0x00, 0x00, v]
+    else if (procent > 2 / 6 && procent <= 3 / 6) {
+        return [0x00, 0xff, igry(procent - 2 / 6)]
     }
-    else if (v < 0xff00) {
-        return [0, v >> 8, 0xff]
-    }else if (v < 0xffff){
-        console.log("a")
-        return [0, 0xff, v << 8]
-    }else if (v < 0xffff00){
-        return [0, 0xff, v << 8]
+    else if (procent > 3 / 6 && procent <= 4 / 6) {
+        return [gry(procent - 3 / 6), 0xff, 0]
     }
-    else {
-        return [0xff, 0xff, 0xff]
+    else if (procent > 4 / 6 && procent <= 5 / 6) {
+        return [0xff, igry(procent - 4 / 6), 0]
     }
+    else if (procent > 5 / 6 && procent <= 6 / 6) {
+        return [0xff, gry(procent - 5 / 6), gry(procent - 5 / 6)]
+    }
+
+    return [0xff, 0xff, 0xff];
 }
 
-export function GetPixColor(realComp: number, imagComp: number, itter = 0xff) {
+export function GetPixColor(realComp: number, imagComp: number, itter = 0xff*2.5) {
     let cr = realComp
     let ci = imagComp
     let color = colorA(100);
@@ -67,27 +73,13 @@ export function GetPixColor(realComp: number, imagComp: number, itter = 0xff) {
         if (rc > itter) {
             const alpha = map(Math.sqrt(n / itter), 0, 1, 0, 255);
             const frac = 255 / 5
-            // console.log({y,x, alpha, rd});
-            // color = color(alpha - frac, alpha - frac * 2, alpha - frac * 3)
-            // if (alpha < frac) {
-            //     color = colorRGB(alpha, 0, 0)            // R
-            // } else if (alpha < frac * 2) {
-            //     color = colorRGB(alpha, alpha - frac, 0) // R G
-            // } else if (alpha < frac * 3) {
-            //     color = colorRGB(0, alpha, 0)            // G
-            // } else if (alpha < frac * 4) {
-            //     color = colorRGB(0, alpha, alpha - frac) // G B
-            // } else if (alpha < frac * 5) {
-            //     color = colorRGB(0, 0, alpha)            // B
-            // } else {
-            //     color = colorA(100)
-            //     console.log(alpha)
-            // }
-            color = colorFrom(alpha, 255);
+            color = colorFrom(alpha, itter);
             break;
         } else {
-            color = colorA(0) // INFI
+            color = [0,0,0] // INFI
         }
     }
+    // return [0xff,0,0xff]
     return color
 }
+
